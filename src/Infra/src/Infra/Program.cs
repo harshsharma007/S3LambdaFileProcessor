517a2bf807deb4eb493feefee3fcf1a100045c8e
Harsh Sharma <harshsharmahs@gmail.com>
2026-05-14 00:19:47 +1000

    Fixing the formating of the file

--- src/Infra/src/Infra/Program.cs
+++ src/Infra/src/Infra/Program.cs
@@ -1,44 +1,40 @@
 ﻿using Amazon.CDK;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace Infra
+namespace Infra;
+
+sealed class Program
 {
-    sealed class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var app = new App();
+        _ = new InfraStack(app, "InfraStack", new StackProps
         {
-            var app = new App();
-            new InfraStack(app, "InfraStack", new StackProps
+            // If you don't specify 'env', this stack will be environment-agnostic.
+            // Account/Region-dependent features and context lookups will not work,
+            // but a single synthesized template can be deployed anywhere.
+
+            // Uncomment the next block to specialize this stack for the AWS Account
+            // and Region that are implied by the current CLI configuration.
+            /*
+            Env = new Amazon.CDK.Environment
             {
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+                Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+                Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+            }
+            */
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-                    Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
-                }
-                */
+            // Uncomment the next block if you know exactly what Account and Region you
+            // want to deploy the stack to.
+            /*
+            Env = new Amazon.CDK.Environment
+            {
+                Account = "123456789012",
+                Region = "us-east-1",
+            }
+            */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = "123456789012",
-                    Region = "us-east-1",
-                }
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-            });
-            app.Synth();
-        }
+            // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+        });
+        app.Synth();
     }
 }
